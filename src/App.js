26dca19ff2eb2688af897db211f29ae89049f539
kishan200 <sarvaiyakishank@gmail.com>
2021-06-24 10:51:58 +0530

    mock api called using axios

--- src/App.js
+++ src/App.js
@@ -4,19 +4,21 @@ import './App.css';
 import Navbar from './components/layout/Navbar';
 import UserItem from './components/users/UserItem';
 import Users from './components/users/Users';
+import axios from 'axios';
 
 class App extends Component {
   state = { 
     users:[],
     loading:false
-    [{}]
+    
   }
     async componentDidMount()
     {
       this.setState({loading:true});
 
-      const res = await fetch('https://api.github.com/users');
-      this.setState({users :res.formData, loading:false});
+      const res = await axios.get('https://api.github.com/users');
+      console.log(res.data)
+      //this.setState({users : res.data, loading:false});
           // .then(res => console.log(res.data));
     }
   render()
--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -2,10 +2,33 @@ import React, { Component } from 'react'
 import UserItem from './UserItem'
 
 class Users extends Component {
-   render() {
+    state = {
+        users:[
+            {
+            id : '1',
+            login :'kishan200',
+            avatar_url :'https://avatars.githubusercontent.com/u/70476064?v=4',
+            html_url :'https://github.com/kishan200'
+            },
+            {
+            id : '2',
+            login :'akash52',
+             avatar_url :'https://avatars.githubusercontent.com/u/31063892?v=4',
+            html_url :'https://github.com/Akash52'
+            },
+            {
+            id : '3',
+            login :'youtubers cafe',
+            avatar_url :'https://avatars.githubusercontent.com/u/76847887?v=4',
+            html_url :'https://github.com/youtuberscafe'
+            },
+        ]
+    }
+
+    render() {
         return (
             <div style={userStyle}>
-               {this.props.users.map(user => (
+               {this.state.users.map(user => (
                    <UserItem key={user.id} user={user} />
                ))} 
             </div>
